6fde6dc72043e46a123e91fbbbd49fec63b12c45
wlstn <jinsoo@naver.com>
2019-11-10 18:12:49 +0900

    apollo-server-koa add

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import { GraphQLServer } from "graphql-yoga";
 
 import typeDefs from "./graphql/typeDefs/mergeSchemas";
 import resolvers from "./graphql/resolvers/mergeResolvers";
+import { ApolloServer, gql } from "apollo-server-koa";
 
 const server = new GraphQLServer({
     typeDefs,
